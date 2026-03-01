380c634fcf28cbc7e72abb95ee0a2b29c940b98e
Lorenzo <lorenzoparentidev@gmail.com>
2026-03-01 17:02:54 +0000

    Finish to migrate styles from SCSS to Tailwind CSS and remove unused SCSS files

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,7 +1,6 @@
 import "vite/modulepreload-polyfill";
 import mountVueApps from "./vuejs/app";
 import "./css/tailwind.css";
-import "./scss/main.scss";
 
 const noJsElements = document.querySelectorAll(".no-js");
 noJsElements.forEach((element) => {
